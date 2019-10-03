985f505f0f2408e3add574b753218254c031ba96
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-03 15:16:38 +0100

    add tests for upper and lower limits

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import * as rtl from '@testing-library/react';
-import Counter from '../Counter';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
+import Counter from "../Counter";
 
 let tools;
 
 beforeEach(() => {
   rtl.cleanup();
-  tools = rtl.render(<Counter user='Peter' />);
+  tools = rtl.render(<Counter user="Peter" countLimit={5} />);
 });
 
-  describe('Counter component', () => {
-  it('can debug the output', () => {
+describe("Counter component", () => {
+  it("can debug the output", () => {
     tools.debug();
   });
 
-  it('shows the correct user', () => {
+  it("shows the correct user", () => {
     const elementWithJoshText = tools.queryByText(/peter/i);
     expect(elementWithJoshText).toBeInTheDocument();
   });
 
-  it('initial count is zero', () => {
+  it("initial count is zero", () => {
     const elementWithZero = tools.queryByText(/0/);
     expect(elementWithZero).toBeInTheDocument();
   });
 
-  it('can increment the count by one by clicking increment', () => {
-    const incButton = tools.queryByTestId('incButton');
+  it("can increment the count by one by clicking increment", () => {
+    const incButton = tools.queryByTestId("incButton");
 
     rtl.fireEvent.click(incButton);
     expect(tools.queryByText(/0/)).not.toBeInTheDocument();
@@ -37,36 +37,46 @@ beforeEach(() => {
     expect(tools.queryByText(/2/)).toBeInTheDocument();
   });
 
-  it('can decrement the count by one by clicking decrement', () => {
-    const decButton = tools.queryByTestId('decButton');
+  it("can decrement the count by one by clicking decrement", () => {
+    const decButton = tools.queryByTestId("decButton");
 
     rtl.fireEvent.click(decButton);
     expect(tools.queryByText(/0/)).not.toBeInTheDocument();
     expect(tools.queryByText(/-1/)).toBeInTheDocument();
-    
   });
 
-  it('can reset the count clicking rest', () => {
-    const resetButton = tools.queryByTestId('resetButton');
+  it("can reset the count clicking rest", () => {
+    const resetButton = tools.queryByTestId("resetButton");
 
     rtl.fireEvent.click(resetButton);
     expect(tools.queryByText(/0/)).toBeInTheDocument();
   });
 
+  it("prevents the count from going over an upper limit", () => {
+    const incButton = tools.queryByTestId("incButton");
 
-  it('prevents the count from going over an upper limit', () => {
+    for (let n = 0; n < 10; n++) {
+      rtl.fireEvent.click(incButton);
+    }
+    expect(tools.queryByText(/5/)).toBeInTheDocument();
+    expect(tools.queryByText(/6/)).not.toBeInTheDocument();
+  });
+
+  it("prevents the count from going under a lower limit", () => {
+    const decButton = tools.queryByTestId("decButton");
+
+    for (let n = 0; n < 10; n++) {
+      rtl.fireEvent.click(decButton);
+    }
+    expect(tools.queryByText(/-5/)).toBeInTheDocument();
+    expect(tools.quertyByText(/-6/)).not.toBeInTheDocument();
+  });
+
+  it("shows a warning once we hit the upper limit of the counter", () => {
     
   });
 
-  it('prevents the count from going under a lower limit', () => {
-    // implement
-  });
-
-  it('shows a warning once we hit the upper limit of the counter', () => {
-    // implement
-  });
-
-  it('shows a warning once we hit the lower limit of the counter', () => {
+  it("shows a warning once we hit the lower limit of the counter", () => {
     // implement
   });
 });
